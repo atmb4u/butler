629138507da411111d282a30a1b5fc9c068e7e2f
Anoop Thomas Mathew <atmb4u@gmail.com>
2014-04-08 09:32:00 +0530

    A lot of consistency issues fixed. key_exists and path_exists works for False responses as well, list len issue fixed, version bumped to v0.8.3a

--- butler/__init__.py
+++ butler/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.8a"
+__version__ = "0.83a"
 __author__ = "Anoop Thomas Mathew"
 __license__ = "BSD"
 __contact__ = "atmb4u at gmail dot com"
@@ -10,6 +10,31 @@ class Butler(object):
         Initiates with the list or dict object obj
         """
         self.obj = obj
+
+    def __getitem__(self, path):
+        """
+        Grab values inside nested dict and list if available, else returns None
+        Returns None if looking up in string.
+        keys: A list of names or ids
+        >>> data1 = Butler({"key": "value"})
+        >>> data1.get(["key"])
+        'value'
+        >>> data2 = Butler([1, 2, 4, 5, [10, 20, 30, 40, 50]])
+        >>> data2.get([4, 3])
+        40
+        >>> data2.get([4, 9])
+
+        >>> data3 = Butler("Hello world")
+        >>> data3.get([6])
+
+        """
+        return_obj = self.obj
+        for key in path:
+            if type(return_obj) in [list, dict]:
+                return_obj = return_obj[key]
+            else:
+                return None
+        return return_obj
 
     def get(self, path):
         """
@@ -27,23 +52,14 @@ class Butler(object):
         >>> data3 = Butler("Hello world")
         >>> data3.get([6])
 
-        Usage:
-
         """
-        return_obj = self.obj
-        for key in path:
-            try:
-                if type(return_obj) in [list, dict]:
-                    return_obj = return_obj[key]
-                else:
-                    return None
-            except LookupError:
-                return None
-        return return_obj
+        try:
+            return self[path]
+        except (LookupError, TypeError):
+            return None
 
     def path_exists(self, path):
         """
-
         >>> data = {'a':1, 'b':2, 'c': {'d': 4, 'e': 5, 'f': [6, 7, 8], 'g':[{'h': 8, 'i': 9, 'j': 10}, {'a':11,
         ... 'b': 12, 'c': 13}]}, 'n': [14, 15, 16, 17, 18]}
 
@@ -53,14 +69,44 @@ class Butler(object):
         False
         >>> quick.path_exists(['c','g',0,'j'])
         True
+        >>> Butler({'tricky': False}).path_exists(['tricky'])
+        True
         """
-        if self.get(path):
+        try:
+            self[path]
             return True
-        else:
+        except KeyError:
             return False
+
+    def key_list(self, sub_dict=None, output=None):
+        """
+        Flattens the sub_dict argument.
+        Internal API used with find and findall functions.
+        >>> data = {'a':10, 'b':[{'c':11, 'd': 13}, {'d':14, 'e':15}]}
+
+        >>> quick = Butler(data)
+
+        >>> quick.flatten(data)
+        [('a', 10), ('c', 11), ('d', 13), ('e', 15), ('d', 14), ('b', [{'c': 11, 'd': 13}, {'e': 15, 'd': 14}])]
+        """
+        if not sub_dict:
+            sub_dict = self.obj
+        if not output:
+            output = []
+        for key in sub_dict:
+            if isinstance(sub_dict[key], dict):
+                self.flatten(sub_dict[key], output)
+            if isinstance(sub_dict[key], list):
+                for sub_dic in sub_dict[key]:
+                    if isinstance(sub_dic, dict):
+                        self.flatten(sub_dic, output)
+            output.append(key)
+        return output
+
 
     def flatten(self, sub_dict=None, output=None):
         """
+        TODO: Make use of generators
         Flattens the sub_dict argument.
         Internal API used with find and findall functions.
         >>> data = {'a':10, 'b':[{'c':11, 'd': 13}, {'d':14, 'e':15}]}
@@ -98,7 +144,10 @@ class Butler(object):
         >>> quick1 = Butler(new_list)
 
         >>> quick1.findall(4)
-        [1, 2, 3, 4]
+        [[1, 2, 3, 4]]
+
+        >>> quick1.findall(5)
+        [5]
         """
         return_list = []
         if isinstance(self.obj, dict):
@@ -109,8 +158,10 @@ class Butler(object):
                         return data
                     return_list.append(data)
         elif isinstance(self.obj, list):  # kept this to keep the API consistent across list and dict
-            if len(self.obj) - 1 > key:
-                return self.obj[key]
+            if len(self.obj) > key:
+                if find:  # API for find() to return the first result
+                    return self.obj[key]
+                return_list.append(self.obj[key])
         else:
             print("findall can be used only with dict or list objects")
         return return_list
@@ -159,8 +210,10 @@ class Butler(object):
         True
         >>> quick.key_exists('w')
         False
+        >>> Butler({'name': False}).key_exists('name')
+        True
         """
-        if self.find(key):
+        if key in self.key_list(self.obj):
             return True
         else:
             return False
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='python-butler',
-      version='0.8a',
+      version='0.8.3a',
       description='Python Dictionaries and Lists on Steroids',
       url='http://github.com/atmb4u/butler',
       author='Anoop Thomas Mathew',
