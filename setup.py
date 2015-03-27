3c095bf599679ad750997ec983f0471ac18edf1c
Anoop Thomas Mathew <atmb4u@gmail.com>
2015-03-27 14:34:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from butler import __version__
 setup(name='python-butler',
       version=__version__,
       description='Python Dictionaries and Lists on Steroids',
-      url='http://github.com/atmb4u/butler',
+      url='https://github.com/atmb4u/butler',
       author='Anoop Thomas Mathew',
       author_email='atmb4u@gmail.com',
       license='BSD',
